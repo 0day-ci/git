5181a262e8f6f77b3270bdd78dd8b432dc6a3b0f
Jonathan Tan <jonathantanmy@google.com>
2017-02-13 16:11:59 -0800

    grep: do not unnecessarily query repo for "--"
    When running a command of the form
    
      git grep --no-index pattern -- path
    
    in the absence of a Git repository, an error message will be printed:
    
      fatal: BUG: setup_git_env called without repository
    
    This is because "git grep" tries to interpret "--" as a rev. "git grep"
    has always tried to first interpret "--" as a rev for at least a few
    years, but this issue was upgraded from a pessimization to a bug in
    commit 59332d1 ("Resurrect "git grep --no-index"", 2010-02-06), which
    calls get_sha1 regardless of whether --no-index was specified. This bug
    appeared to be benign until commit b1ef400 ("setup_git_env: avoid blind
    fall-back to ".git"", 2016-10-20) when Git was taught to die in this
    situation.  (This "git grep" bug appears to be one of the bugs that
    commit b1ef400 is meant to flush out.)
    
    Therefore, always interpret "--" as signaling the end of options,
    instead of trying to interpret it as a rev first.
    
    Signed-off-by: Jonathan Tan <jonathantanmy@google.com>

--- t/t7810-grep.sh
+++ t/t7810-grep.sh
@@ -982,6 +982,18 @@ test_expect_success 'grep -e -- -- path' '
 	test_cmp expected actual
 '
 
+test_expect_success 'grep --no-index pattern -- path' '
+	rm -fr non &&
+	mkdir -p non/git &&
+	(
+		GIT_CEILING_DIRECTORIES="$(pwd)/non" &&
+		export GIT_CEILING_DIRECTORIES &&
+		cd non/git &&
+		echo hello >hello &&
+		git grep --no-index o -- .
+	)
+'
+
 cat >expected <<EOF
 hello.c:int main(int argc, const char **argv)
 hello.c:	printf("Hello world.\n");
