d5d35b31d42c91ab3da12ea79206eb7397c8d8f9
Jeff King <peff@peff.net>
2017-03-15 17:29:08 -0400

    fast-import: replace fixed buffer with odb_pack_name
    This removes the possibility of truncating the filename
    without realizing it.
    
    Signed-off-by: Jeff King <peff@peff.net>

--- fast-import.c
+++ fast-import.c
@@ -966,15 +966,15 @@ static char *keep_pack(const char *curr_index_name)
 
 static void unkeep_all_packs(void)
 {
-	static char name[PATH_MAX];
+	struct strbuf name = STRBUF_INIT;
 	int k;
 
 	for (k = 0; k < pack_id; k++) {
 		struct packed_git *p = all_packs[k];
-		snprintf(name, sizeof(name), "%s/pack/pack-%s.keep",
-			 get_object_directory(), sha1_to_hex(p->sha1));
-		unlink_or_warn(name);
+		odb_pack_name(&name, p->sha1, "keep");
+		unlink_or_warn(name.buf);
 	}
+	strbuf_release(&name);
 }
 
 static int loosen_small_pack(const struct packed_git *p)
