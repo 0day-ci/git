a4b17b2c4aed06fd0651c09f9393353acdd43b06
brian m. carlson <sandals@crustytoothpaste.net>
2017-03-26 16:01:28 +0000

    builtin/receive-pack: fix incorrect pointer arithmetic
    If we had already processed the last newline in a push certificate, we
    would end up subtracting NULL from the end-of-certificate pointer when
    computing the length of the line.  This would have resulted in an
    absurdly large length, and possibly a buffer overflow.  Instead,
    subtract the beginning-of-certificate pointer from the
    end-of-certificate pointer, which is what's expected.
    
    Note that this situation should never occur, since not only do we
    require the certificate to be newline terminated, but the signature will
    only be read from the beginning of a line.  Nevertheless, it seems
    prudent to correct it.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- builtin/receive-pack.c
+++ builtin/receive-pack.c
@@ -1527,7 +1527,7 @@ static void queue_commands_from_cert(struct command **tail,
 
 	while (boc < eoc) {
 		const char *eol = memchr(boc, '\n', eoc - boc);
-		tail = queue_command(tail, boc, eol ? eol - boc : eoc - eol);
+		tail = queue_command(tail, boc, eol ? eol - boc : eoc - boc);
 		boc = eol ? eol + 1 : eoc;
 	}
 }
