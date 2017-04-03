1ca2c03e3ebc1e7a3f408022b3e72fc706359bee
Jeff Hostetler <jeffhost@microsoft.com>
2017-04-03 18:53:06 +0000

    p0002-read-cache: test core.checksumindex
    Teach t/perf/p0002-read-cache to time read_cache() with
    and without the index checksum calculation.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- t/perf/p0002-read-cache.sh
+++ t/perf/p0002-read-cache.sh
@@ -7,7 +7,13 @@ test_description="Tests performance of reading the index"
 test_perf_default_repo
 
 count=1000
-test_perf "read_cache/discard_cache $count times" "
+test_perf "read_cache/discard_cache checksum=1 $count times" "
+	git config --local core.checksumindex 1 &&
+	test-read-cache $count
+"
+
+test_perf "read_cache/discard_cache checksum=0 $count times" "
+	git config --local core.checksumindex 0 &&
 	test-read-cache $count
 "
 
