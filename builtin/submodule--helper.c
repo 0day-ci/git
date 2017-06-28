b35d20bd5c7d8dc8a75768b35eaed5204a1ae860
Junio C Hamano <gitster@pobox.com>
2017-06-28 13:38:48 -0700

    submodule--helper: do not call utf8_fprintf() unnecessarily
    The helper function utf8_fprintf(fp, ...) has exactly the same
    effect to the output stream fp as fprintf(fp, ...) does, and the
    only difference is that its return value counts in display columns
    consumed (assuming that the payload is encoded in UTF-8), as opposed
    to number of bytes.
    
    There is no reason to call it unless the caller cares about its
    return value.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/submodule--helper.c
+++ builtin/submodule--helper.c
@@ -326,7 +326,7 @@ static int module_list(int argc, const char **argv, const char *prefix)
 			printf("%06o %s %d\t", ce->ce_mode,
 			       oid_to_hex(&ce->oid), ce_stage(ce));
 
-		utf8_fprintf(stdout, "%s\n", ce->name);
+		fprintf(stdout, "%s\n", ce->name);
 	}
 	return 0;
 }
@@ -1038,7 +1038,7 @@ static int update_clone(int argc, const char **argv, const char *prefix)
 		return 1;
 
 	for_each_string_list_item(item, &suc.projectlines)
-		utf8_fprintf(stdout, "%s", item->string);
+		fprintf(stdout, "%s", item->string);
 
 	return 0;
 }
