68285a19ea90bcb8c3d07b608578775e5ff674f4
Stefan Beller <sbeller@google.com>
2017-07-10 18:06:39 -0700

    RFC: A new type of symbolic refs
    A symbolic ref can currently only point at (another symbolic) ref.
    However are use cases for symbolic refs to point at other things
    representing a commit-ish, such as gitlink entries in other
    repositories.  In this use case we can use such a symbolic link
    to strengthen the relationship between a submodule and a superproject.
    Examples:
    
    1) It makes it easier to explain when we recurse into submodules and
       to which sha1 the submodule is updated.
    
       Currently a submodule (or any repo) is either on a branch (i.e.
       HEAD is a symbolic ref) or is in 'detached HEAD' state (HEAD is
       a direct ref).
       For submodules it is wrong to be on its own branch if it wants to be
       controlled by the superproject as being on its own branch signals that
       the submodule is independent and can move the HEAD freely.
       Being in 'detached HEAD' state is the alternative to that and was
       chosen when git-submodule was implemented, but it is equally wrong;
       the lesser of two evils.
    
       Semantically the correct way to state a submodule is part of the
       superproject is by pointing its HEAD to the superproject.
    
       We do have "reset/checkout --recurse-submodules" now, but it is
       hard to explain what actually happens there (Which submodules are
       updated? All of them! -- But I want a subset only!)
    
       With this new mode of symbolic refs, any submodule that tracks the
       superproject, would 'automatically follow' the superproject as the
       submodules HEAD moves when the superproject changes.
    
    2) "git -C submodule commit" would behave the same as it would on branch
       nowadays: It would make the commit in the submodule and then change
       the target of the symbolic ref which would be the index of the
       superproject! That implies that you do not need to 'git add' the
       submodule to the superproject, but have it done automatically.
    
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- submodule.c
+++ submodule.c
@@ -2037,3 +2037,43 @@ int submodule_to_gitdir(struct strbuf *buf, const char *submodule)
 cleanup:
 	return ret;
 }
+
+int read_external_symref(struct strbuf *from, struct strbuf *out)
+{
+	struct child_process cp = CHILD_PROCESS_INIT;
+	const char *repo, *gitlink;
+	int hint, code;
+	struct strbuf **split = strbuf_split(from, 0);
+	struct strbuf cmd_out = STRBUF_INIT;
+
+	if (!split[0] || !split[1])
+		return -1;
+
+	repo = split[0]->buf + 5; /* skip 'repo:' */
+	gitlink = split[1]->buf;
+
+	argv_array_pushl(&cp.args,
+			"ignored-first-arg",
+			"-C", repo,
+			"ls-tree", "-z", "HEAD", "--", gitlink, NULL);
+
+	/*
+	 * 17 accounts for '160000 commit ',
+	 * the \t before path and trailing \0.
+	 */
+	hint = 17 + GIT_SHA1_HEXSZ + split[1]->len;
+	code = capture_command(&cp, &cmd_out, hint);
+
+	strbuf_release(split[0]);
+	strbuf_release(split[1]);
+
+	if (!code) {
+		strbuf_reset(out);
+		strbuf_add(out, cmd_out.buf + strlen("160000 commit "),
+			   GIT_SHA1_HEXSZ);
+	} else
+		return -1;
+
+	return 0;
+}
+
